49a8e807f3c7e39a7e29fcc6039db7a02947c9d7
Rafa Pagés <blinkmenow@gmail.com>
2015-03-25 14:24:58 +0100

    Camera reader now works perfectly

--- camera.cpp
+++ camera.cpp
@@ -12,8 +12,47 @@ Camera::~Camera()
 
 void Camera::readCameraParameters(const std::string &_textline){
 
-//    sscanf(_textline.c_str(), "%f %f %f %s",);
+    std::stringstream line(_textline);
 
+    // Intrinsic parameters
+    for (unsigned int i = 0; i < 3; i++){
+        for (unsigned int j = 0; j < 3; j++){
+            float value;
+            line >> value;
+            this->K_(i,j) = value;
+        }
+    }
+
+    // Extrinsic parameteres
+    for (unsigned int i = 0; i < 3; i++){
+        for (unsigned int j = 0; j < 3; j++){
+            float value;
+            line >> value;
+            this->R_(i,j) = value;
+        }
+    }
+
+    // Camera position
+    for (unsigned int i = 0; i < 3; i++){
+        float value;
+        line >> value;
+        this->position_(i) = value;
+    }
+
+    // Image dimensions
+    unsigned int uivalue;
+    line >> uivalue;
+    this->imWidth = uivalue;
+    line >> uivalue;
+    this->imHeight = uivalue;
+
+//    std::cerr << "K matrix: " << std::endl;
+//    std::cerr << this->K_ << std::endl;
+//    std::cerr << "R matrix: " << std::endl;
+//    std::cerr << this->R_ << std::endl;
+//    std::cerr << "position: " << std::endl;
+//    std::cerr << this->position_ << std::endl;
+//    std::cerr << "Image: " << this->imWidth << "x" << this->imHeight << std::endl;
 
 }
 
